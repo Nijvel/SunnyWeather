3eb9da40a2ce46e0b218096d354cac2a994fc889
Nijvel <y567_1051@163.com>
2021-11-09 16:03:55 +0800

    实现搜索城市数据

---
+++ app/src/main/java/com/sunnyweather/android/logic/network/SunnyWeatherNetwork.kt
@@ -0,0 +1,46 @@
+package com.sunnyweather.android.logic.network
+
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.await
+import java.lang.RuntimeException
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+object SunnyWeatherNetwork {
+
+    private val placeService = ServiceCreator.create<PlaceService>()
+
+    // 在协程作用域中调用此函数，通过await函数将调用的协程挂起，当网络请求返回的时候，唤醒被挂起的协程
+    suspend fun searchPlaces(query: String) = placeService.searchPlaces(query).await()
+
+    // 因为不同的Service接口返回的数据类型可能不同，所以写成这样方便复用。定义成Call<T>的扩展函数，也是方便调用。
+    // 在await函数中直接通过enqueue进行网络请求
+    private suspend fun <T> Call<T>.await(): T {
+        /**
+         * suspendCoroutine必须在协程作用域或挂起函数中才能调用。
+         * 作用：将当前协程立刻挂起，然后在一个普通的线程中执行Lambda表达式中的代码。
+         * 在Lambda中通过调用continuation的resume()和resumeWithException()让协程恢复运行
+         */
+        return suspendCoroutine { continuation ->
+            enqueue(object : Callback<T> {
+                override fun onResponse(call: Call<T>, response: Response<T>) {
+                    val body = response.body()
+                    if (body != null) {
+                        // 如果获取body成功，则恢复外部被挂起的协程
+                        continuation.resume(body)
+                    } else {
+                        continuation.resumeWithException(RuntimeException("response body is null"))
+                    }
+                }
+
+                override fun onFailure(call: Call<T>, t: Throwable) {
+                    continuation.resumeWithException(t)
+                }
+            })
+        }
+    }
+
+}
---
+++ app/src/main/java/com/sunnyweather/android/ui/place/PlaceFragment.kt
@@ -0,0 +1,74 @@
+package com.sunnyweather.android.ui.place
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.sunnyweather.android.R
+import com.sunnyweather.android.logic.model.Place
+
+class PlaceFragment : Fragment(), LifecycleObserver {
+    val viewModel by lazy {
+        ViewModelProvider(this).get(PlaceViewModel::class.java)
+    }
+
+    private lateinit var adapter: PlaceAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_place, container, false)
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+     fun onCreate() {
+        val layoutManager = LinearLayoutManager(activity)
+        val recyclerView: RecyclerView? = activity?.findViewById(R.id.recyclerView)
+        val searchPlaceEdit: EditText? = activity?.findViewById(R.id.searchPlaceEdit)
+        val bgImageView: ImageView? = activity?.findViewById(R.id.bgImageView)
+        recyclerView?.layoutManager = layoutManager
+        adapter = PlaceAdapter(this, viewModel.placeList)
+        recyclerView?.adapter = adapter
+        searchPlaceEdit?.addTextChangedListener { editable ->
+            val content = editable.toString()
+            if (content.isNotEmpty()) {
+                viewModel.searchPlaces(content)
+            } else {
+                recyclerView?.visibility = View.GONE
+                bgImageView?.visibility = View.VISIBLE
+                viewModel.placeList.clear()
+                adapter.notifyDataSetChanged()
+            }
+        }
+        viewModel.placeLiveData.observe(this, Observer { result ->
+            val places = result.getOrNull()
+            if (places != null) {
+                recyclerView?.visibility = View.VISIBLE
+                bgImageView?.visibility = View.GONE
+                viewModel.placeList.clear()
+                viewModel.placeList.addAll(places)
+                adapter.notifyDataSetChanged()
+            } else {
+                Toast.makeText(activity, "未查询到任何地点", Toast.LENGTH_SHORT).show()
+                result.exceptionOrNull()?.printStackTrace()
+            }
+        })
+        activity?.lifecycle?.removeObserver(this)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        activity?.lifecycle?.addObserver(this)
+    }
+}
